1fddeda3ede3a9eb54c85839e5154a535f4c3647
warmcreators-us <115068126+warmcreators-us@users.noreply.github.com>
2022-10-05 14:47:50 +0530

    Updated the normal dependencies to load more efficiently

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -89,3 +89,24 @@ export default function Home({
 }
 
 Home.Layout = Layout
+{
+  "name": "@vercel/commerce-shopify",
+  "version": "0.0.1",
+  "license": "MIT",
+  "scripts": {
+    "release": "taskr release",
+    "build": "taskr build",
+    "dev": "taskr",
+    "types": "tsc --emitDeclarationOnly",
+    "prettier-fix": "prettier --write .",
+    "generate": "DOTENV_CONFIG_PATH=./.env graphql-codegen -r dotenv/config"
+  },
+  "sideEffects": false,
+  "type": "module",
+  "exports": {
+    ".": "./dist/index.js",
+    "./*": [
+      "./dist/*.js",
+      "./dist/*/index.js"
+    ],
+    "./next.config": "./dist/next.config.cjs"
